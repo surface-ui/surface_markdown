2bf651851452a4f543bae8a70db96535ac2bc572
Marlus Saraiva <marlus.saraiva@gmail.com>
2023-05-29 11:35:23 -0300

    Support surface v0.10

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule SurfaceMarkdown.MixProject do
   use Mix.Project
 
-  @version "0.5.1"
+  @version "0.6.0"
 
   def project do
     [
@@ -33,7 +33,7 @@ defmodule SurfaceMarkdown.MixProject do
       {:ex_doc, ">= 0.28.5", only: :docs},
       {:floki, "~> 0.33.1", only: :test},
       {:jason, "~> 1.0"},
-      {:surface, "~>0.8.0 or ~>0.9.0"},
+      {:surface, ">=0.8.0 and < 0.11.0"},
       {:earmark, "~> 1.4"}
     ]
   end
